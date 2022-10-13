0b3e7ac35c3bd59625b880b153538f685f981040
zzangjyj0818 <jyj010818@naver.com>
2022-10-13 22:32:47 +0900

    초기 커밋.

---
+++ Array_Stack/Array_Stack.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+typedef int element;
+element stack[MAX_SIZE];
+int top = -1;
+
+int is_empty() {
+	return (top == -1);
+}
+int is_full() {
+	return (top == (MAX_SIZE - 1));
+}
+void push(element item) {
+	if (is_full()) {
+		exit(1);
+	}
+	else
+		stack[++top] = item;
+}
+element pop() {
+	if (is_empty())
+		exit(1);
+	else
+		return stack[top--];
+}
+element peek() {
+	if (is_empty())
+		exit(1);
+	else
+		return stack[top];
+}
+int main() {
+	push(1);
+	push(2);
+	push(3);
+	printf("%d\n",pop());
+	printf("%d\n", pop());
+	printf("%d\n", pop());
+}
