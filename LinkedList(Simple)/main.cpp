9b2b7f8eb9359c3c84dd8f3f987e43a7c5dc3072
Jeong Yeon Jae <jyj010818@naver.com>
2022-10-24 20:16:05 +0900

    Initial Commit

---
+++ LinkedList(Simple)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList(Simple)
+//
+//  Created by Jeong on 2022/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
