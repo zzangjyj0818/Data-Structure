fe56d0909d3055d3c58ead027dd8547cbad319b3
Jeong Yeon Jae <jyj010818@naver.com>
2022-10-25 22:32:04 +0900

    Initial Commit

---
+++ LinkedList(Doubly)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinkedList(Doubly)
+//
+//  Created by Jeong on 2022/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
